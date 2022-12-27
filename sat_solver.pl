8ec1022f13a68eb7977cb831358e41963c5e0f7e
Silas Kraume <silas.kraume1552@gmail.com>
2022-12-27 17:07:00 +0100

    first ideas for normalize and to_cnf

--- sat_solver.pl
+++ sat_solver.pl
@@ -50,27 +50,59 @@ implies(lit(false), lit(false)) :-
     lit(true).
 
 
-exactly_one_pos(ListOfVars) :-
-    count(lit(true), ListOfVars, 1).
+exactly_one_pos(ListOfVars).
 
-count(_, [], 0).
-count(X, [X|T], N) :- 
-    count(X, T, M), 
-    N is M+1.
-count(X, [_|T], N) :- 
-    count(X, T, N).
+min_one_pos(ListOfVars).
 
-min_one_pos(ListOfVars) :-
-    member(lit(true), ListOfVars).
     
-
+normalise(lit(X), lit(X)).
+normalise(equivalence(A, B), Res) :-
+    normalise(A, NA),
+    normalise(B, NB),
+    Res = and(or(not(NA), NB), or(NA, not(NB))).
+normalise(implies(A, B), Res) :-
+    normalise(A, NA),
+    normalise(B, NB),
+    Res = or(not(NA), NB).
+normalise(and(A, B), and(NA, NB)) :-
+    normalise(A, NA),
+    normalise(B, NB).
+normalise(or(A, B), or(NA, NB)) :-
+    normalise(A, NA),
+    normalise(B, NB).
+normalise(not(A), not(NA)) :-
+    normalise(A, NA).
 %% normalise(+Formula, -NFormula).
-normalise(_Formula, _NFormula) :-
-    true.
+% normalise(_Formula, _NFormula) :-
+%     true.
+to_cnf(Formula, CNF) :-
+    normalise(Formula, Normalised),
+    to_cnf_helper(Normalised, CNF).
+
+to_cnf_helper(lit(X), [[X]]).
+to_cnf_helper(not(lit(X)), [[not(X)]]).
+to_cnf_helper(not(or(A, B)), CNF) :-
+    to_cnf_helper(not(A), CNF1),
+    to_cnf_helper(not(B), CNF2),
+    append(CNF1, CNF2, CNF).
+to_cnf_helper(and(A, B), CNF) :-
+    to_cnf_helper(A, CNF1),
+    to_cnf_helper(B, CNF2),
+    append(CNF1, CNF2, CNF).
+to_cnf_helper(or(A, B), [[A|Rest]|Tail]) :-
+    to_cnf_helper(A, [[A|Rest]|_]),
+    to_cnf_helper(B, Tail).
+to_cnf_helper(or(A, B), [[B|Rest]|Tail]) :-
+    to_cnf_helper(B, [[B|Rest]|_]),
+    to_cnf_helper(A, Tail).
+to_cnf_helper(or(A, B), [[A, B|Rest]|Tail]) :-
+    to_cnf_helper(A, [[A|_]|_]),
+    to_cnf_helper(B, [[B|Rest]|_]),
+    to_cnf_helper(A, Tail).
 
 %% to_cnf(+Formula, -CNF).
-to_cnf(_Formula, _CNF) :-
-    true.
+% to_cnf(_Formula, _CNF) :-
+%     true.
 
 %% solve(+CNF).
 solve(_CNF) :-
