ea8abf2ad40f2e230216bf7f048ccf773a97644c
Silas Kraume <silas.kraume1552@gmail.com>
2023-01-04 20:04:30 +0100

    reworked formula normalisation

--- sat_solver.pl
+++ sat_solver.pl
@@ -3,106 +3,46 @@
 :- load_test_files([]).
 :- use_module(library(lists)).
 
-lit(true) :-
-    true.
-lit(false) :-
-    false.
+normalise(lit(L), lit(L)) :- !.
+normalise(equivalence(P, Q), and(or(not(P1), Q1), or(not(Q1), P1))) :- !,
+    normalise(P, P1),
+    normalise(Q, Q1).
+normalise(implies(P, Q), or(not(P1), Q1)) :- !,
+    normalise(P, P1),
+    normalise(Q, Q1).
+normalise(and(P, Q), and(P1, Q1)) :- !,
+    normalise(P, P1),
+    normalise(Q, Q1).
+normalise(or(P, Q), or(P1, Q1)) :- !,
+    normalise(P, P1),
+    normalise(Q, Q1).
+normalise(not(P), not(P1)) :- !,
+    normalise(P, P1).
 
-not(lit(true)) :-
-    lit(false).
-not(lit(false)) :-
-    lit(true).
+normalise(min_one_pos(List), NFormula) :-
+	normalise_list(List, NList),
+	normalise_min_one_pos(NList, NFormula).
 
-and(lit(true), lit(true)) :-
-    lit(true).
-and(lit(true), lit(false)) :-
-    lit(false).
-and(lit(false), lit(true)) :-
-    lit(false).
-and(lit(false), lit(false)) :-
-    lit(false).
+normalise(exactly_one_pos(List), NFormula) :-
+	normalise_list(List, NList),
+	normalise_exactly_one_pos(NList, NFormula).
 
-or(lit(true), lit(true)) :-
-    lit(true).
-or(lit(true), lit(false)) :-
-    lit(true).
-or(lit(false), lit(true)) :-
-    lit(true).
-or(lit(false), lit(false)) :-
-    lit(false).
+normalise_min_one_pos([], lit(false)).
+normalise_min_one_pos([H|T], or(H, Rest)) :-
+	normalise_min_one_pos(T, Rest).
 
-equivalence(lit(true), lit(true)) :-
-    lit(true).
-equivalence(lit(true), lit(false)) :-
-    lit(false).
-equivalence(lit(false), lit(true)) :-
-    lit(false).
-equivalence(lit(false), lit(false)) :-
-    lit(true).
+normalise_exactly_one_pos([], lit(false)).
+normalise_exactly_one_pos([H|T], or(H, and(not(H), Rest))) :-
+	normalise_exactly_one_pos(T, Rest).
 
-implies(lit(true), lit(true)) :-
-    lit(true).
-implies(lit(true), lit(false)) :-
-    lit(false).
-implies(lit(false), lit(true)) :-
-    lit(true).
-implies(lit(false), lit(false)) :-
-    lit(true).
-
-
-exactly_one_pos(ListOfVars).
-
-min_one_pos(ListOfVars).
-
-    
-normalise(lit(X), lit(X)).
-normalise(equivalence(A, B), Res) :-
-    normalise(A, NA),
-    normalise(B, NB),
-    Res = and(or(not(NA), NB), or(NA, not(NB))).
-normalise(implies(A, B), Res) :-
-    normalise(A, NA),
-    normalise(B, NB),
-    Res = or(not(NA), NB).
-normalise(and(A, B), and(NA, NB)) :-
-    normalise(A, NA),
-    normalise(B, NB).
-normalise(or(A, B), or(NA, NB)) :-
-    normalise(A, NA),
-    normalise(B, NB).
-normalise(not(A), not(NA)) :-
-    normalise(A, NA).
-%% normalise(+Formula, -NFormula).
-% normalise(_Formula, _NFormula) :-
-%     true.
-to_cnf(Formula, CNF) :-
-    normalise(Formula, Normalised),
-    to_cnf_helper(Normalised, CNF).
-
-to_cnf_helper(lit(X), [[X]]).
-to_cnf_helper(not(lit(X)), [[not(X)]]).
-to_cnf_helper(not(or(A, B)), CNF) :-
-    to_cnf_helper(not(A), CNF1),
-    to_cnf_helper(not(B), CNF2),
-    append(CNF1, CNF2, CNF).
-to_cnf_helper(and(A, B), CNF) :-
-    to_cnf_helper(A, CNF1),
-    to_cnf_helper(B, CNF2),
-    append(CNF1, CNF2, CNF).
-to_cnf_helper(or(A, B), [[A|Rest]|Tail]) :-
-    to_cnf_helper(A, [[A|Rest]|_]),
-    to_cnf_helper(B, Tail).
-to_cnf_helper(or(A, B), [[B|Rest]|Tail]) :-
-    to_cnf_helper(B, [[B|Rest]|_]),
-    to_cnf_helper(A, Tail).
-to_cnf_helper(or(A, B), [[A, B|Rest]|Tail]) :-
-    to_cnf_helper(A, [[A|_]|_]),
-    to_cnf_helper(B, [[B|Rest]|_]),
-    to_cnf_helper(A, Tail).
+normalise_list([], []).
+normalise_list([H|T], [NH|NT]) :-
+	normalise(H, NH),
+	normalise_list(T, NT).
 
 %% to_cnf(+Formula, -CNF).
-% to_cnf(_Formula, _CNF) :-
-%     true.
+to_cnf(_Formula, _CNF) :-
+    true.
 
 %% solve(+CNF).
 solve(_CNF) :-
